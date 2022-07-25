0d66fb297213f175f375612a7ea88be722c4d38f
João Seckler <jseckler@riseup.net>
2022-07-25 17:11:30 -0300

    Implement AnalisadorDeAprovacao

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,26 @@
 package domain
 
+import domain.criterios.Basico
+import domain.criterios.CriterioDeAprovacao
+import domain.criterios.Intermediario
+import domain.criterios.Rigoroso
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+  private lateinit var cda: CriterioDeAprovacao
 
+  fun defineCriterio(criterio: CriterioDeAprovacao) {
+    cda = criterio
+  }
+
+  fun fechaBoletim(boletim: Boletim): BoletimFechado {
+    var bf: BoletimFechado = BoletimFechado(
+      boletim.mediaEPs,
+      boletim.mediaMiniEPs,
+      cda.mediaFinal(boletim),
+      cda.estaAprovado(boletim)
+    )
+
+    return bf
+  }
 }
